700bf97c3ba2a66c05bd6b270f83e84b89004542
praveen dodla <dodlapraveen09@gmail.com>
2022-08-19 18:14:48 +0530

    Added Unordered list method

---
+++ DataStructurePrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DataStructuresPractice;
+
+namespace DataStructurePrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputFilePath = @"G:\BridgeLabz\DataStructuresPrograms\DataStructurePrograms\InputFile.txt";
+            string outputFilePath = @"G:\BridgeLabz\DataStructuresPrograms\DataStructurePrograms\OutputFile.txt";
+            new UnorderedList().UnorderListOperations(inputFilePath,outputFilePath);
+        }
+    }
+}
